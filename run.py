8ed19c77c9b17c0835e4da9d35d9b137d95f005d
Vlad Boldyrev <boldyrev2040@gmail.com>
2021-04-11 21:44:14 +0300

    Изменена структуру скриптов

--- run.py
+++ run.py
@@ -1,8 +1,6 @@
 from todolist import create_app, db_session
-from flask_ngrok import run_with_ngrok
 
 app = create_app()
-run_with_ngrok(app)
 
 if __name__ == '__main__':
     # Необходимо поместить базу данных в данный католог
