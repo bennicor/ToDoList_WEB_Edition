500e6ca2518b179ef8ec35afdbe726e3870658e4
G4st3r21 <aporyadin003@gmail.com>
2021-04-11 22:21:14 +0300

    flash-уведомления

--- run.py
+++ run.py
@@ -2,9 +2,9 @@ from todolist import create_app, db_session
 from flask_ngrok import run_with_ngrok
 
 app = create_app()
-run_with_ngrok(app)
+# run_with_ngrok(app)
 
 if __name__ == '__main__':
     # Необходимо поместить базу данных в данный католог
     db_session.global_init("todolist/db/TDLDataBase.db")
-    app.run()
+    app.run(debug=True)
