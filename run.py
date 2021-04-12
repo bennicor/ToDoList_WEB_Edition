be788228aec51f791b5e49c6d64e9ddedeca0695
G4st3r21 <aporyadin003@gmail.com>
2021-04-13 00:04:04 +0300

    hotfix

--- run.py
+++ run.py
@@ -1,12 +1,10 @@
-import os
 from todolist import create_app, db_session
-from flask_ngrok import run_with_ngrok
+# from flask_ngrok import run_with_ngrok
 
 app = create_app()
-run_with_ngrok(app)
+# run_with_ngrok(app)
 
 if __name__ == '__main__':
     # Необходимо поместить базу данных в данный католог
     db_session.global_init("todolist/db/TDLDataBase.db")
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run()
